bd9958b009690de6256a3ae0edac0decfe1e7db5
Nathan Oertel <nathan@digitalclouddesigns.com>
2019-09-24 18:44:45 +0000

    Corrected the class name

--- src/model/ItemRetireResponse.php
+++ src/model/ItemRetireResponse.php
@@ -3,7 +3,7 @@ namespace WalmartSellerAPI\model;
 
 use WalmartSellerAPI\model\AbstractModel;
 
-class ItemResponses extends AbstractModel {
+class ItemRetireResponse extends AbstractModel {
 
     public function __construct($data = null) {
         parent::__construct('responses/ItemRetireResponse', $data);
