5a17e35c3c4cf6a3e8436eb876f431e765f07ba9
Nathan Oertel <nathan@digitalclouddesigns.com>
2019-09-24 09:41:34 -0400

    Added the mapping of the response name to the corresponding object

--- src/ShipConfirmResponse.php
+++ src/ShipConfirmResponse.php
@@ -2,4 +2,12 @@
 namespace WalmartSellerAPI;
 
 class ShipConfirmResponse extends AbstractResponse {
+    protected function getModel($name) {
+        switch($name) {
+            case 'order':
+                return 'WalmartSellerAPI\model\Order';
+            default:
+                throw new Exception('OrderAcknowledgementResponse '.$name.' Not Found');
+        }
+    }    
 }
