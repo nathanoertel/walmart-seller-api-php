b5319c3de44a7a0009fc80a5c9362e07d2433b29
Nathan Oertel <nathan@digitalclouddesigns.com>
2020-04-03 12:26:59 +0000

    Added lower case version of name and mapping to correct model based on XML response

--- src/InventoryResponse.php
+++ src/InventoryResponse.php
@@ -2,4 +2,12 @@
 namespace WalmartSellerAPI;
 
 class InventoryResponse extends AbstractResponse {
+    protected function getModel($name) {
+        switch($name) {
+            case 'inventory':
+                return 'WalmartSellerAPI\model\Inventory';
+            default:
+                throw new Exception('InventoryResponse '.$name.' Not Found');
+        }
+    }    
 }
--- src/model/Inventory.php
+++ src/model/Inventory.php
@@ -6,7 +6,10 @@ use WalmartSellerAPI\model\AbstractModel;
 class Inventory extends AbstractModel {
 
     public function __construct($data = null) {
-        parent::__construct('inventory/Inventory', $data);
+        parent::__construct(array(
+            'inventory/Inventory',
+            'inventory/inventory'
+        ), $data);
     }
 }
 ?>
