1c1607258b495821edfde5a1e7aca3e811b58b4c
Nathan Oertel <nathan@invergehq.com>
2024-09-23 08:42:10 -0400

    feat: added json option for inventory

---
+++ src/InventoryRequestJSON.php
@@ -0,0 +1,28 @@
+<?php
+namespace WalmartSellerAPI;
+
+class InventoryRequestJSON extends AbstractJSONRequest {
+	public function find($sku) {
+		return $this->get('', array(
+			'sku' => $sku
+		));
+	}
+
+	public function update($sku, $quantity) {
+		return $this->put('?sku='.$sku, json_encode([
+			'sku' => $sku,
+			'quantity' => [
+				'unit' => 'EACH',
+				'amount' => $quantity
+			],
+		]));
+	}
+
+	public function getEndpoint() {
+		return '/v3/inventory';
+	}
+
+	protected function getResponse() {
+		return 'WalmartSellerAPI\InventoryResponseJSON';
+	}
+}
---
+++ src/InventoryResponseJSON.php
@@ -0,0 +1,5 @@
+<?php
+namespace WalmartSellerAPI;
+
+class InventoryResponseJSON extends AbstractJSONResponse {
+}
